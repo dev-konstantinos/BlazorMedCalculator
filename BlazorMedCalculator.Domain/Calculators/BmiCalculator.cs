7030687ec477a4afc7be81b05d604c89aef5e10e
dev-konstantinos <dasistwundervoll@gmail.com>
2026-01-31 11:23:30 +0100

    Calculator logic and validation are separated. Exceptions prevented.

--- BlazorMedCalculator.Domain/Calculators/BmiCalculator.cs
+++ BlazorMedCalculator.Domain/Calculators/BmiCalculator.cs
@@ -4,12 +4,6 @@
     {
         public static double Calculate(double weightKg, double heightCm)
         {
-            if (weightKg <= 0)
-                throw new ArgumentOutOfRangeException(nameof(weightKg));
-
-            if (heightCm <= 0)
-                throw new ArgumentOutOfRangeException(nameof(heightCm));
-
             var heightM = heightCm / 100.0;
             return weightKg / (heightM * heightM);
         }
